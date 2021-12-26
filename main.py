54bb2e612746c1a36eb1526e72a7f0b8c4da3e09
onnya <timur.kvashin.a@gmail.com>
2021-12-26 21:55:52 +0300

    Fix running

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, make_response, jsonify
+import os
 
 
 app = Flask(__name__)
@@ -15,7 +16,8 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 def main():
-    app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
